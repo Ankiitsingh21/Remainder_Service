d162f9f09b31c4b65f29046c84c7bd6b794f6c65
Ankit Singh <ankiitsingh21@gmail.com>
2024-02-14 17:26:09 +0530

    Initial Setup Done

---
+++ src/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyparser = require('body-parser');
+const {PORT} = require('./config/serverConfig');
+
+const setupAndServer = ()=>{
+        const app = express();
+        app.use(bodyparser.json());
+        app.use(bodyparser.urlencoded({extended:true}));
+
+        app.listen(PORT,()=>{
+                console.log(`Server started at PORT : ${PORT}`);
+        });
+}
+
+setupAndServer();
